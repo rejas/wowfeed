4f35ed4a4fa161e425db0672d8fdf267e4d227ac
rejas <github@veeck.de>
2015-01-22 21:55:17 +0100

    cleanups

--- utils.js
+++ utils.js
@@ -68,3 +68,13 @@ exports.pick = function (obj, keys) {
         return cumm
     }, {})
 };
+
+//
+exports.capitalize = function (string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+};
+
+//
+exports.sortData = function (a, b) {
+    return (b.date - a.date);
+};
