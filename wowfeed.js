b7556750f1e6e18d8bd6b9f6c65255c3c433c692
Veeck <github@veeck.de>
2019-10-09 16:39:27 +0200

    Use async/await (#12)
    * Change some Promises to async/await
    
    * Update eslint and dependencies
    
    * Remove useless comma

--- wowfeed.js
+++ wowfeed.js
@@ -45,7 +45,8 @@ const fs    = require('fs'),
 
         initialize: () => {
             // Create and start the server to handle requests
-            http.createServer((request, response) => {
+            const server = http.createServer();
+            server.on('request', async (request, response) => {
                 let analytics = ua('UA-431999-5', {https: true});
 
                 // Extract the searchquery from the url
@@ -71,26 +72,29 @@ const fs    = require('fs'),
                     options.realm = options.realm.replace('\'', '');
                 }
 
-                // Actually create the feed
-                app.createFeed(options)
-                    .then(feed => {
-                        // Send analytics events
-                        if (options.character) {
-                            analytics.pageview(`character/${options.region}/${options.realm}/${options.character}`).send();
-                        } else if (options.guild) {
-                            analytics.pageview(`guild/${options.region}/${options.realm}/${options.guild}`).send();
-                        }
-                        return wowfeed.createFeedPage(response, feed);
-                    })
-                    .catch(error => {
-                        // Send analytics exception
-                        analytics.exception({
-                            dp: request.url,
-                            exd: `${error.response.status} - ${error.response.statusText}`
-                        }).send();
-                        wowfeed.createErrorPage(response, error);
-                    });
-            }).listen(port);
+                try {
+                    // Actually create the feed
+                    const feed = await app.createFeed(options);
+
+                    // Send analytics events
+                    if (options.character) {
+                        analytics.pageview(`character/${options.region}/${options.realm}/${options.character}`).send();
+                    } else if (options.guild) {
+                        analytics.pageview(`guild/${options.region}/${options.realm}/${options.guild}`).send();
+                    }
+
+                    return wowfeed.createFeedPage(response, feed);
+                } catch (error) {
+                    // Send analytics exception
+                    analytics.exception({
+                        dp: request.url,
+                        exd: `${error.response.status} - ${error.response.statusText}`
+                    }).send();
+
+                    wowfeed.createErrorPage(response, error);
+                }
+            });
+            server.listen(port);
 
             console.log('Server running at port: ' + port);
         }
